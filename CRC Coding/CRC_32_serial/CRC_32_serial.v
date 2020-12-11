e5af7cb8a665ac340f5c3da8b7c999c9f068cad2
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-12-12 01:18:09 +0530

    Update CRC_32_serial.v

--- CRC Coding/CRC_32_serial/CRC_32_serial.v
+++ CRC Coding/CRC_32_serial/CRC_32_serial.v
@@ -12,22 +12,22 @@ reg [4:0] count; //
 parameter idle = 2'b00; //
 parameter compute = 2'b01; //
 parameter finish = 2'b10; //
-  always@ (posedge clk) //executed each time the clk is triggered for a positive edge
-begin // can be seen as the upper parenthesis in the c language, and end can be seen as the lower parenthesis
-case (state) // Select case in state 
- idle: begin // is the wait state
- if (load) //load signal enters the compute state effectively
+  always@ (posedge clk) //
+begin // 
+case (state) // 
+ idle: begin // 
+ if (load) //
  state <= compute; 
  else
  state <= idle; 
  end
- compute:begin //d_finish signal enters the finish state effectively
+ compute:begin //
  if(d_finish) 
  state <= finish; 
  else
  state <= compute; 
  end
- finish: begin // Determines whether the data in the 16 stagings is fully output
+ finish: begin // 
    if(count==32) 
  state <= idle; 
  else
@@ -35,10 +35,9 @@ case (state) // Select case in state
  end
 endcase
 end 
-always@ (posedge clk or negedge rst)//whenever the clk positive trigger or rst negative edge trigger is executed
+always@ (posedge clk or negedge rst)//
  if(rst) 
  begin 
- //crc_reg the initial value < pre-installed b0000_0000_0000_0000 15:0, 15:0, 16'
  count <= 5'b0_0000; 
  state <= idle; 
  end
@@ -54,11 +53,11 @@ always@ (posedge clk or negedge rst)//whenever the clk positive trigger or rst n
    crc_reg[2] <= crc_reg[1] ^ crc_reg[31] ^ crc_in; 
    crc_reg[30:3] <= crc_reg[29:2]; 
    crc_reg[31] <= crc_reg[30] ^ crc_reg[31] ^ crc_in; 
- crc_out <= crc_in; // The input is the output
+ crc_out <= crc_in; //
  end 
  finish:begin 
-   crc_out <= crc_reg[31]; // Stager 15 as output
-   crc_reg[31:0] <= {crc_reg[30:0],1'b0}; // Shift.
+   crc_out <= crc_reg[31]; 
+   crc_reg[31:0] <= {crc_reg[30:0],1'b0}; 
  end 
  endcase 
 endmodule
