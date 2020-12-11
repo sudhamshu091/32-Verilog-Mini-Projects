f99c1038dd1a0834027033ad33d91d3550528cb1
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-12-11 20:58:43 +0530

    Update booth_multiplier.v

--- Booth Multiplication/booth_multiplier.v
+++ Booth Multiplication/booth_multiplier.v
@@ -1,31 +1,40 @@
-module booth(X, Y, Z,en);
-  input signed [15:0] X, Y;
-  input en;
-  output signed [31:0] Z;
-  reg signed [31:0] Z;
-  reg [1:0] temp;
-  
-integer i;
-  reg E1;
-  reg [15:0] Y1;
-always @ (X, Y,en)
-  begin
-  Z = 32'd0;
-  E1 = 1'd0;
-  Y1 = - Y;
-  Z[15:0]=X;
-  for (i = 0; i < 16; i = i + 1)
-    begin
-    temp = {X[i], E1};
-    case (temp)
-    2'd2 : Z [31 : 16] = Z [31 : 16] + Y1;
-    2'd1 : Z [31 : 16] = Z [31 : 16] + Y;
-    default : begin end
-    endcase
-Z = Z >> 1;
-Z[31] = Z[30];
-E1 = X[i];
+module booth_algorithm(prod, busy, mc, mp, clk, start);
+output [15:0] prod;
+output busy;
+input [7:0] mc, mp;
+input clk, start;
+reg [7:0] A, Q, M;
+reg Q_1;
+reg [3:0] count;
+wire [7:0] sum, difference;
+always @(posedge clk)
+begin
+ if (start) begin
+ A <= 8'b0;
+ M <= mc;
+ Q <= mp;
+ Q_1 <= 1'b0;
+ count <= 4'b0;
+ end
+ else begin
+ case ({Q[0], Q_1})
+ 2'b0_1 : {A, Q, Q_1} <= {sum[7], sum, Q};
+ 2'b1_0 : {A, Q, Q_1} <= {difference[7], difference, Q};
+ default: {A, Q, Q_1} <= {A[7], A, Q};
+ endcase
+ count <= count + 1'b1;
+ end
 end
+alu adder (sum, A, M, 1'b0);
+alu subtracter (difference, A, ~M, 1'b1);
+assign prod = {A, Q};
+assign busy = (count < 8);
+endmodule
 
-end
+module alu(out, a, b, cin);
+output [7:0] out;
+input [7:0] a;
+input [7:0] b;
+input cin;
+assign out = a + b + cin;
 endmodule
