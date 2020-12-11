2f81d450e7fea9ce94e95d1b298c22e5f22143c9
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-12-11 20:59:11 +0530

    Update booth_multiplier_tb.v

--- Booth Multiplication/booth_multiplier_tb.v
+++ Booth Multiplication/booth_multiplier_tb.v
@@ -1,26 +1,20 @@
-`timescale 1ns / 1ps
-
 module booth_algorithm_tb;
-
-	reg [15:0] X;
-	reg [15:0] Y;
-	reg en;
-
-	wire [31:0] Z;
-	booth uut (
-		.X(X), 
-		.Y(Y), 
-		.Z(Z), 
-		.en(en)
-	);
-
-	initial begin
-		X = 15;
-		Y = 16;
-		en = 1;
-
-		#100 $finish;
-
-	end
-      
+reg clk, start;
+reg [7:0] a, b;
+wire [15:0] ab;
+wire busy;
+booth_algorithm mult(ab, busy, a, b, clk, start);
+initial begin
+clk = 0;
+$display("first example: a = 3 b = 17");
+a = 3; b = 17; start = 1; #50 start = 0;
+#80 $display("first example done");
+$display("second example: a = 7 b = 7");
+a = 7; b = 7; start = 1; #50 start = 0;
+#80 $display("second example done");
+$finish;
+end
+always #5 clk = !clk;
+always @(posedge clk) $strobe("ab: %d busy: %d at time=%t", ab, busy,
+$stime);
 endmodule
