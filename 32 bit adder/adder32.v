563b4380f101ae8bf9d6c6eea0808adefe9d6d50
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-12-15 23:00:13 +0530

    Create adder32.v

---
+++ 32 bit adder/adder32.v
@@ -0,0 +1,11 @@
+module Adder32 (A, B, Ci, S, Co);
+input[31:0] A;
+input[31:0] B;
+input Ci;
+output[31:0] S;
+output Co;
+wire[32:0] Sum33;
+assign Sum33 = A + B + Ci ;
+assign S = Sum33[31:0] ;
+assign Co = Sum33[32] ;
+endmodule
