ee2e1ca7a4dca86c16c385b01961edef9de6f8f0
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-12-31 20:33:04 +0530

    Update fixed_point_subtractor.v

--- Fixed Point Adder and Subtractor/fixed_point_subtractor.v
+++ Fixed Point Adder and Subtractor/fixed_point_subtractor.v
@@ -16,7 +16,7 @@ assign c = res;
 always @(a,b) begin
 	// both negative or both positive
 	if(a[N-1] == b[N-1]) begin						//	Since they have the same sign, absolute magnitude increases
-		res[N-2:0] = a[N-2:0] + b[N-2:0];		//		So we just add the two numbers
+		res[N-2:0] = a[N-2:0] - b[N-2:0];		//		So we just add the two numbers
 		res[N-1] = a[N-1];							//		and set the sign appropriately...  Doesn't matter which one we use, 
 															//		they both have the same sign
 															//	Do the sign last, on the off-chance there was an overflow...  
@@ -24,11 +24,11 @@ always @(a,b) begin
 	//	one of them is negative...
 	else if(a[N-1] == 0 && b[N-1] == 1) begin		//	subtract a-b
 		if( a[N-2:0] > b[N-2:0] ) begin					//	if a is greater than b,
-			res[N-2:0] = a[N-2:0] - b[N-2:0];			//		then just subtract b from a
+			res[N-2:0] = a[N-2:0] + b[N-2:0];			//		then just subtract b from a
 			res[N-1] = 0;										//		and manually set the sign to positive
 			end
 		else begin												//	if a is less than b,
-			res[N-2:0] = b[N-2:0] - a[N-2:0];			//		we'll actually subtract a from b to avoid a 2's complement answer
+			res[N-2:0] = b[N-2:0] + a[N-2:0];			//		we'll actually subtract a from b to avoid a 2's complement answer
 			if (res[N-2:0] == 0)
 				res[N-1] = 0;										//		I don't like negative zero....
 			else
@@ -37,14 +37,14 @@ always @(a,b) begin
 		end
 	else begin												//	subtract b-a (a negative, b positive)
 		if( a[N-2:0] > b[N-2:0] ) begin					//	if a is greater than b,
-			res[N-2:0] = a[N-2:0] - b[N-2:0];			//		we'll actually subtract b from a to avoid a 2's complement answer
+			res[N-2:0] = a[N-2:0] + b[N-2:0];			//		we'll actually subtract b from a to avoid a 2's complement answer
 			if (res[N-2:0] == 0)
 				res[N-1] = 0;										//		I don't like negative zero....
 			else
 				res[N-1] = 1;										//		and manually set the sign to negative
 			end
 		else begin												//	if a is less than b,
-			res[N-2:0] = b[N-2:0] - a[N-2:0];			//		then just subtract a from b
+			res[N-2:0] = b[N-2:0] + a[N-2:0];			//		then just subtract a from b
 			res[N-1] = 0;										//		and manually set the sign to positive
 			end
 		end
