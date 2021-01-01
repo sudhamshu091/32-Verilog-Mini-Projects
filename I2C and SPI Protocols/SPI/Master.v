ed281d8da8c93c11a8a8d33d42b81602d1217815
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-01-01 11:31:10 +0530

    Update Master.v

--- I2C and SPI Protocols/SPI/Master.v
+++ I2C and SPI Protocols/SPI/Master.v
@@ -1,1 +1,32 @@
-
+module Master(qw,scl,mosi,miso,ss1,ss2,sel);
+input qw;
+output reg scl;
+input miso;
+output reg [0:0]mosi;
+output reg ss1;
+output reg ss2;
+output reg [1:0] sel;
+reg [16:0]xx;
+initial begin
+scl = 0;
+ss1 = 1;
+ss2 = 1;
+#10 ss1 = 0;
+sel = 0;
+xx = 17'b011101001_00011001;
+#100 ss2 = 0;
+xx = 17'b010101011_00011001;
+ss1 = 1;
+sel = 1;
+end
+always @(negedge ss1 or negedge ss2)
+slock ;
+always @(posedge scl)begin
+mosi = xx[16];
+xx = xx<<1;
+end
+task slock;
+repeat (38)
+#2 scl = !scl;
+endtask: slock;
+endmodule
