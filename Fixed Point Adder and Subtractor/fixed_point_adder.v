d1252884a08aee1f4294500f7fb9feeaa149cfea
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-01-13 23:19:42 +0530

    Update fixed_point_adder.v

--- Fixed Point Adder and Subtractor/fixed_point_adder.v
+++ Fixed Point Adder and Subtractor/fixed_point_adder.v
@@ -15,41 +15,42 @@ assign c = res;
 
 always @(a,b) begin
 	// both negative or both positive
-	if(a[N-1] == b[N-1]) begin						//	Since they have the same sign, absolute magnitude increases
-		res[N-2:0] = a[N-2:0] + b[N-2:0];		//		So we just add the two numbers
-		res[N-1] = a[N-1];							//		and set the sign appropriately...  Doesn't matter which one we use, 
-															//		they both have the same sign
-															//	Do the sign last, on the off-chance there was an overflow...  
-		end												//		Not doing any error checking on this...
+	if(a[N-1] == b[N-1]) begin				
+		res[N-2:0] = a[N-2:0] + b[N-2:0];	 	
+		res[N-1] = a[N-1];				
+															
+															  
+		end												
 	//	one of them is negative...
-	else if(a[N-1] == 0 && b[N-1] == 1) begin		//	subtract a-b
-		if( a[N-2:0] > b[N-2:0] ) begin					//	if a is greater than b,
-			res[N-2:0] = a[N-2:0] - b[N-2:0];			//		then just subtract b from a
-			res[N-1] = 0;										//		and manually set the sign to positive
+	else if(a[N-1] == 0 && b[N-1] == 1) begin		
+		if( a[N-2:0] > b[N-2:0] ) begin					
+			res[N-2:0] = a[N-2:0] - b[N-2:0];			
+			res[N-1] = 0;										
 			end
-		else begin												//	if a is less than b,
-			res[N-2:0] = b[N-2:0] - a[N-2:0];			//		we'll actually subtract a from b to avoid a 2's complement answer
+		else begin												
+			res[N-2:0] = b[N-2:0] - a[N-2:0];			
 			if (res[N-2:0] == 0)
-				res[N-1] = 0;										//		I don't like negative zero....
+				res[N-1] = 0;										
 			else
-				res[N-1] = 1;										//		and manually set the sign to negative
+				res[N-1] = 1;										
 			end
 		end
-	else begin												//	subtract b-a (a negative, b positive)
-		if( a[N-2:0] > b[N-2:0] ) begin					//	if a is greater than b,
-			res[N-2:0] = a[N-2:0] - b[N-2:0];			//		we'll actually subtract b from a to avoid a 2's complement answer
+	else begin												
+		if( a[N-2:0] > b[N-2:0] ) begin					
+			res[N-2:0] = a[N-2:0] - b[N-2:0];			
 			if (res[N-2:0] == 0)
-				res[N-1] = 0;										//		I don't like negative zero....
+				res[N-1] = 0;										
 			else
-				res[N-1] = 1;										//		and manually set the sign to negative
+				res[N-1] = 1;										
 			end
-		else begin												//	if a is less than b,
-			res[N-2:0] = b[N-2:0] - a[N-2:0];			//		then just subtract a from b
-			res[N-1] = 0;										//		and manually set the sign to positive
+		else begin												
+			res[N-2:0] = b[N-2:0] - a[N-2:0];			
+			res[N-1] = 0;										
 			end
 		end
 	end
 endmodule
+
 module Test_add;
 
 	// Inputs
@@ -95,15 +96,15 @@ module Test_add;
         
 		// Add stimulus here
 		forever begin
-			#1 a = a+5179347;			//	why not?
-			a[31] = 0;					//	a is negative...
+			#1 a = a+5179347;			
+			a[31] = 0;					// a is negative...
 			b[31] = 1;
 			
 			
-			if (a[30:0] > 2.1E9)			//	input will always be "positive"
+			if (a[30:0] > 2.1E9)				// input will always be "positive"
 				begin
 					a = 0;
-					b[31] = 1;				//	b is negative...
+					b[31] = 1;			// b is negative...
 					b[30:0] = b[30:0] + 3779351;
 				end
 		end
