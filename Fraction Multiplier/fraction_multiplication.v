c611c4d2e01ecaa62001794419da0ae38a86b02d
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-01-14 14:18:21 +0530

    Update fraction_multiplication.v

--- Fraction Multiplier/fraction_multiplication.v
+++ Fraction Multiplier/fraction_multiplication.v
@@ -1,6 +1,5 @@
-
 `define M B[0]
- module mult2C (CLK, St, Mplier, Mcand, Product, Done);
+ module fraction_multiplication (CLK, St, Mplier, Mcand, Product, Done);
  input CLK;
  input St;
  input[3:0] Mplier;
