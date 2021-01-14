b9a8498c2053a2f5b521eb9a8a736a3c3c39ef26
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-01-14 18:59:11 +0530

    Update LFSR.v

--- LFSR and CFSR/LFSR.v
+++ LFSR and CFSR/LFSR.v
@@ -1,4 +1,3 @@
-
 module lfsr (out, clk, rst);
 
   output reg [3:0] out;
@@ -15,28 +14,4 @@ always @(posedge clk, posedge rst)
     else
       out = {out[2:0],feedback};
   end
-endmodule
-
-module lfsr_tb();
-reg clk_tb;
-reg rst_tb;
-wire [3:0] out_tb;
-
-initial
-begin
-    clk_tb = 0;
-    rst_tb = 1;
-    #15;
-    
-    rst_tb = 0;
-    #200;
-end
-
-always
-begin
-    #5;
-    clk_tb = ~ clk_tb;
-end
-
-lfsr DUT(out_tb,clk_tb,rst_tb);
 endmodule
